1a92c05c24335b99ccb6dbfcbc869f1df5da8221
Vitaly Blinovskov <blinovskov@yandex-team.ru>
2016-10-10 19:51:24 +0500

    do not push history if url is the same

--- src/middleware.js
+++ src/middleware.js
@@ -1,13 +1,23 @@
 import { ACTION_PREFIX, ACTION_TYPES, HISTORY_METHODS} from './constants';
+import { parsePath } from 'history';
 
 export default ({ history }) => ({ dispatch, getState }) => next => action => {
 
-    const { type, payload } = action;
+    if (action.type.indexOf(ACTION_PREFIX) === 0 && action.type !== ACTION_TYPES.LOCATION_CHANGED) {
 
-    if (type.indexOf(ACTION_PREFIX) === 0 && type !== ACTION_TYPES.LOCATION_CHANGED) {
+        if ([ACTION_TYPES.PUSH, ACTION_TYPES.REPLACE].includes(action.type)) {
 
-        if (HISTORY_METHODS[type]) {
-            history[HISTORY_METHODS[type]](payload);
+            action.payload = typeof action.payload === 'string' ? parsePath(action.payload) : action.payload;
+
+            const sameLocation = history.location.pathname  === action.payload.pathname
+                              && history.location.search === action.payload.search
+                              && history.location.hash   === action.payload.hash;
+
+            action.type = sameLocation ? ACTION_TYPES.REPLACE : action.type
+        }
+
+        if (HISTORY_METHODS[action.type]) {
+            history[HISTORY_METHODS[action.type]](action.payload);
         }
 
         return;
