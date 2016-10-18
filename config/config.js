217bcf5d6b45686055c795e66ceca5758de46460
Vitaly Blinovskov <blinovskov@yandex-team.ru>
2016-10-18 18:31:33 +0500

    rename umd lib

--- config/config.js
+++ config/config.js
@@ -2,6 +2,6 @@ module.exports = {
     SRC_PATH:'src',
     BUILD_PATH: 'dist',
     BUILD_PATH_UMD: 'umd',
-    LIB_NAME: 'ReduxTinyRouter',
+    LIB_NAME: 'ReduxUnityRouter',
     LIB_FILENAME: 'redux-unity-router',
 };
