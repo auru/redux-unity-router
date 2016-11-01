00857ba1ce8714ee83038813a0c76cbc240bf852
Vitaly Blinovskov <blinovskov@yandex-team.ru>
2016-11-01 16:42:51 +0500

    [fix] export actions

--- src/index.js
+++ src/index.js
@@ -10,6 +10,7 @@ export const History = {
 export { default as ACTION_TYPES } from './actionTypes';
 export { default as actionTypes } from './actionTypes';
 export { default as createRouter } from './router';
+export * as actions from './actions';
 export { Link } from './components';
 export { RouterProvider } from './components';
 export { Fragment } from './components';
