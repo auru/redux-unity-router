a99a677e0668003cc31a29f02cd43dd48d485728
blackheart <blackheart@yandex-team.ru>
2017-10-21 15:47:19 +0500

    chore: impore exmaple

--- examples/src/components/App/App.js
+++ examples/src/components/App/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './App.css';
 
 import { Link, RouterProvider, Fragment } from '../../../../dist';
@@ -7,6 +8,7 @@ import { LINK_MATCH_PARTIAL, LINK_MATCH_EXACT } from '../../../../dist/constants
 import routes from '../../routes';
 
 import Lorem from '../Lorem';
+import CurrentRoute from '../CurrentRoute';
 
 const Main = ({children}) => {
     return (
@@ -15,6 +17,10 @@ const Main = ({children}) => {
             {children}
         </div>
     );
+};
+
+Main.propTypes = {
+    children: PropTypes.any
 };
 
 const onClickCallback = () => {
@@ -31,86 +37,87 @@ const delayedOnClickCallback = e => {
 
 class App extends Component {
 
-  constructor(props) {
-      super(props);
+    constructor(props) {
+        super(props);
 
-      this.handleDelayedRedirect = this.handleDelayedRedirect.bind(this);
+        this.handleDelayedRedirect = this.handleDelayedRedirect.bind(this);
 
-      this.state = {
-          redirect: null
-      };
-  }
+        this.state = {
+            redirect: null
+        };
+    }
 
-  handleDelayedRedirect() {
-      setTimeout(() => {
-          this.setState({
-              redirect: { id: 'DelayedRedirected' }
-          });
-      }, 2000);
-  }
+    handleDelayedRedirect() {
+        setTimeout(() => {
+            this.setState({
+                redirect: { id: 'DelayedRedirected' }
+            });
+        }, 2000);
+    }
 
-  render() {
-    return (
-    <RouterProvider routes={routes} slice="router" immutable>
-        <div className="App container">
-            <div className="app__navigation">
-                <Link to="/main" activeMatch={LINK_MATCH_EXACT}>Main</Link>
-                <Link to="/main/splat" activeMatch={LINK_MATCH_EXACT}>Default</Link>
-                <Link to="/main/another_splat" activeMatch={/^\/main\/another/}>Another default</Link>
-                <Link to={{ pathname: '/main/user', query: { userId: 1, edit: false }, hash: 'title' }} activeMatch={LINK_MATCH_EXACT}>User</Link>
-                <Link to="/main/user?edit=true&userId=2#title" activeMatch={LINK_MATCH_EXACT}>User 2</Link>
-                <Link to="/scroll-to-hash#h2" activeMatch={LINK_MATCH_EXACT}>Scroll to hash</Link>
-                <Link to={{ id: 'Settings' }} activeMatch={LINK_MATCH_EXACT}>Settings</Link>
-                <Link to={{ id: 'Preferences', params: { action: 'edit' }, query: { edit: true, sections: ['main','side'] }, hash: 'title' }} activeMatch={LINK_MATCH_EXACT}>Preferences</Link>
-                <Link to="/redirect" activeMatch={LINK_MATCH_PARTIAL}>Redirect</Link>
-                <Link to="/delredirect" activeMatch={LINK_MATCH_PARTIAL} onClick={this.handleDelayedRedirect}>Delayed redirect</Link>
-                <Link to="/on-click" activeMatch={LINK_MATCH_EXACT} onClick={onClickCallback}>onClick</Link>
-                <Link to="/on-click-promise" activeMatch={LINK_MATCH_EXACT} onClick={delayedOnClickCallback}>onClick Promise</Link>
-                <Link to="https://ya.ru" target="_blank">External</Link>
-            </div>
+    render() {
+        return (
+            <RouterProvider routes={routes} slice="router" immutable>
+                <div className="App container">
+                    <div className="app__navigation">
+                        <Link to="/main" activeMatch={LINK_MATCH_EXACT}>Main</Link>
+                        <Link to="/main/splat" activeMatch={LINK_MATCH_EXACT}>Default</Link>
+                        <Link to="/main/another_splat" activeMatch={/^\/main\/another/}>Another default</Link>
+                        <Link to={{ pathname: '/main/user', query: { userId: 1, edit: false }, hash: 'title' }} activeMatch={LINK_MATCH_EXACT}>User</Link>
+                        <Link to="/main/user?edit=true&userId=2#title" activeMatch={LINK_MATCH_EXACT}>User 2</Link>
+                        <Link to="/scroll-to-hash#h2" activeMatch={LINK_MATCH_EXACT}>Scroll to hash</Link>
+                        <Link to={{ id: 'Settings' }} activeMatch={LINK_MATCH_EXACT}>Settings</Link>
+                        <Link to={{ id: 'Preferences', params: { action: 'edit' }, query: { edit: true, sections: ['main','side'] }, hash: 'title' }} activeMatch={LINK_MATCH_EXACT}>Preferences</Link>
+                        <Link to="/redirect" activeMatch={LINK_MATCH_PARTIAL}>Redirect</Link>
+                        <Link to="/delredirect" activeMatch={LINK_MATCH_PARTIAL} onClick={this.handleDelayedRedirect}>Delayed redirect</Link>
+                        <Link to="/on-click" activeMatch={LINK_MATCH_EXACT} onClick={onClickCallback}>onClick</Link>
+                        <Link to="/on-click-promise" activeMatch={LINK_MATCH_EXACT} onClick={delayedOnClickCallback}>onClick Promise</Link>
+                        <Link to="https://ya.ru" target="_blank">External</Link>
+                    </div>
 
-            <div className="app__content">
-                <Fragment id="Main" component={Main}>
-                    <Fragment id="User">
-                        User content
-                        <Fragment id="UserEdit">
-                            Edit form
+                    <div className="app__content">
+                        <CurrentRoute />
+                        <Fragment id="Main" component={Main}>
+                            <Fragment id="User">
+                                User content
+                                <Fragment id="UserEdit">
+                                    Edit form
+                                </Fragment>
+                            </Fragment>
+                            <Fragment id="default">
+                                Default
+                            </Fragment>
                         </Fragment>
-                    </Fragment>
-                    <Fragment id="default">
-                        Default
-                    </Fragment>
-                </Fragment>
-                <Fragment id="ScrollToHash" component={Lorem} />
-                <Fragment id="Settings">
-                    <h2>Settings</h2>
-                </Fragment>
-                <Fragment id="Preferences">
-                    <h2>Preferences</h2>
-                </Fragment>
-                <Fragment id="OnClick">
-                    <h2>OnClick</h2>
-                </Fragment>
-                <Fragment id="OnClickPromise">
-                    <h2>OnClick Promise</h2>
-                </Fragment>
-                <Fragment id="Redirect" redirect={{ id: 'Redirected' }}>
-                    <Fragment id="Redirected">
-                        <h2>Redirected</h2>
-                        You have been redirected here
-                    </Fragment>
-                </Fragment>
-                <Fragment id="DelayedRedirect" redirect={this.state.redirect}>
-                    <Fragment id="DelayedRedirected">
-                        <h2>Delayed Redirected</h2>
-                        You have been redirected here
-                    </Fragment>
-                </Fragment>
-            </div>
-          </div>
-      </RouterProvider>
-    );
-  }
+                        <Fragment id="ScrollToHash" component={Lorem} />
+                        <Fragment id="Settings">
+                            <h2>Settings</h2>
+                        </Fragment>
+                        <Fragment id="Preferences">
+                            <h2>Preferences</h2>
+                        </Fragment>
+                        <Fragment id="OnClick">
+                            <h2>OnClick</h2>
+                        </Fragment>
+                        <Fragment id="OnClickPromise">
+                            <h2>OnClick Promise</h2>
+                        </Fragment>
+                        <Fragment id="Redirect" redirect={{ id: 'Redirected' }}>
+                            <Fragment id="Redirected">
+                                <h2>Redirected</h2>
+                                You have been redirected here
+                            </Fragment>
+                        </Fragment>
+                        <Fragment id="DelayedRedirect" redirect={this.state.redirect}>
+                            <Fragment id="DelayedRedirected">
+                                <h2>Delayed Redirected</h2>
+                                You have been redirected here
+                            </Fragment>
+                        </Fragment>
+                    </div>
+                </div>
+            </RouterProvider>
+        );
+    }
 }
 
 export default App;
